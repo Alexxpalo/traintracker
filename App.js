8160225746f3f5dd3a8564b828f2fa3cd8e724a9
Alexxpalo <palosaaritobias@gmail.com>
2023-03-08 16:53:18 +0200

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [fetchedData, setFetchedData] = React.useState([]);
+  const [trainid, setTrainid] = React.useState('');
+  const [filteredData, setFilteredData] = React.useState([]);
+
+  const fetchTrainData = () => {
+    console.log("fetching train data")
+    const currentTimeUTC = new Date().toISOString();
+    fetch(`https://rata.digitraffic.fi/api/v1/trains/latest/${trainid}`)
+      .then((response) => response.json())
+      .then((json) => setFetchedData(json[0]))
+      .then(() => console.log(fetchedData))
+      .then(() => console.log(currentTimeUTC))
+      .then(() => {
+        const filtered = fetchedData.timeTableRows.filter((row) => {
+          return row.liveEstimateTime > currentTimeUTC && row.trainStopping === true;
+        });
+        setFilteredData(filtered);
+      })
+      .then(() => console.log(filteredData))
+      .catch((error) => console.error(error))
+      .finally(() => console.log('done'));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-green-700">
+      <Text className="text-white text-xl">Enter train number</Text>
+      <TextInput className="border-2 border-white rounded-md p-2 text-white text-lg w-1/4 text-center my-6"
+      onChangeText={setTrainid}
+      value={trainid}
+      placeholder='123'
+      keyboardType='numeric'
+      />
+      <TouchableOpacity className="border border-green-800 px-6 py-2 bg-green-900 rounded-3xl"
+      onPress={fetchTrainData}
+      >
+        <Text className="text-white text-xl">Search</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
