03af0931bac690d487cb9848dd2110d5d16a87ef
Alexxpalo <palosaaritobias@gmail.com>
2023-03-09 12:27:57 +0200

    Added train animation and background.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { ImageBackground, Text, View, TextInput, TouchableOpacity, ScrollView, Image, Animated, Dimensions } from 'react-native';
 
 export default function App() {
   const [fetchedData, setFetchedData] = useState([]);
@@ -7,11 +7,14 @@ export default function App() {
   const [filteredData, setFilteredData] = useState([]);
   const [trainStops, setTrainStops] = useState([]);
   const [stopCodes, setStopCodes] = useState({});
+  const translation = useRef(new Animated.Value(0)).current;
+  const [animationDone, setAnimationDone] = useState(false);
 
   useEffect(() => {
-    fetch(`https://rata.digitraffic.fi/api/v1/metadata/stations`)
-      .then((response) => response.json())
-      .then((json) => {
+    const fetchStations = async () => {
+      try {
+        const response = await fetch(`https://rata.digitraffic.fi/api/v1/metadata/stations`);
+        const json = await response.json();
         const stopCodes = json.reduce((acc, station) => {
           if (station.type === 'STATION') {
             acc[station.stationShortCode] = station.stationName;
@@ -19,66 +22,91 @@ export default function App() {
           return acc;
         }, {});
         setStopCodes(stopCodes);
-      })
-      .then (() => console.log(stopCodes))
-      .catch((error) => console.error(error))
-      .finally(() => console.log('done'));
+        console.log('Stations fetched');
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchStations();
   }, []);
 
-  const fetchTrainData = () => {
-    setTrainStops([]);
-    console.log("fetching train data")
-    const currentTimeUTC = new Date().toISOString();
-    fetch(`https://rata.digitraffic.fi/api/v1/trains/latest/${trainid}`)
-      .then((response) => response.json())
-      .then((json) => setFetchedData(json[0]))
-      .then(() => {
-        const filtered = fetchedData.timeTableRows.filter((row) => {
-          if (row.liveEstimateTime > currentTimeUTC && row.trainStopping === true) {
-            const timeString = new Date(row.liveEstimateTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-            setTrainStops((trainStops) => [...trainStops, [row.stationShortCode, row.type, timeString]]);
-          }
-          return row.liveEstimateTime > currentTimeUTC && row.trainStopping === true;
-        });
-        setFilteredData(filtered);
-      })
-      .then(() => console.log(trainStops))
-      .catch((error) => console.error(error))
-      .finally(() => console.log('done'));
+  const animateTrain = () => {
+    setAnimationDone(false);
+    const screenWidth = Dimensions.get('screen').width;
+    Animated.timing(translation, {
+      toValue: screenWidth + 382,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start(() => {
+      setAnimationDone(true);
+      console.log(animationDone);
+    });
   };
 
-  return (
-    <View className="flex-1 bg-green-700">
-      <View className="flex-1 items-center justify-center">
+
+  const fetchTrainData = async () => {
+    translation.setValue(0);
+    setTrainStops([]);
+    const currentTimeUTC = new Date().toISOString();
+
+    try {
+      const response = await fetch(`https://rata.digitraffic.fi/api/v1/trains/latest/${trainid}`);
+      const json = await response.json();
+      setFetchedData(json[0]);
+      const filteredData = json[0].timeTableRows.filter((row) => {
+        if (row.liveEstimateTime > currentTimeUTC && row.trainStopping === true) {
+          const timeString = new Date(row.liveEstimateTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+          setTrainStops((trainStops) => [...trainStops, [row.stationShortCode, row.type, timeString]]);
+        }
+        return row.liveEstimateTime && row.liveEstimateTime > currentTimeUTC;
+      });
+      setFilteredData(filteredData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+return (
+  <View className="flex-1 bg-green-700">
+    <Image className="absolute z-0 w-full h-full"
+      source={require('./assets/background.png')}
+    />
+    <View className="flex-1 items-center justify-center">
       <Text className="text-white text-xl">Enter train number</Text>
       <TextInput className="border-2 border-white rounded-md p-2 text-white text-lg w-1/4 text-center my-6"
-      onChangeText={setTrainid}
-      value={trainid}
-      placeholder='123'
-      keyboardType='numeric'
+        onChangeText={setTrainid}
+        value={trainid}
+        placeholder='123'
+        keyboardType='numeric'
       />
       <TouchableOpacity className="border border-green-800 px-6 py-2 bg-green-900 rounded-3xl"
-      onPress={fetchTrainData}
+        onPress={() => { fetchTrainData(); animateTrain(); }}
       >
         <Text className="text-white text-xl">Search</Text>
       </TouchableOpacity>
-      </View>
-      <ScrollView className="flex-1 px-10">
-      {
-          trainStops.map((stop, index) => {
-            return (
-              <View className="flex items-center my-3 border-b-2 border-b-green-400" key={index}>
+    </View>
+    <Animated.Image className="absolute scale-50 z-10 -left-[382] top-[350]"
+      source={require('./assets/train.png')}
+      style={{ transform: [{ translateX: translation }] }}
+    />
+    <ScrollView className="flex-1 px-10">
+      {animationDone ?
+        trainStops.map((stop, index) => {
+          return (
+            <View className="flex items-center my-3 border-b-2 border-b-green-400" key={index}>
               <Text className="text-white text-2xl basis-2/5 mb-2">{stopCodes[stop[0]]}</Text>
               <View className="flex flex-row">
                 <Text className="text-white text-xl flex">{stop[2]}</Text>
                 <Text className="text-white text-xl basis-2/5 text-right">{stop[1]}</Text>
               </View>
-              </View>
-            )
-          }
+            </View>
           )
         }
-        </ScrollView>
-    </View>
-  );
+        ) : null
+      }
+    </ScrollView>
+  </View>
+);
 }
